c5f769939fa69ce24140200447ae78d15c0730fd
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-13 09:44:51 +0200

    Add project files.

---
+++ KyhLoginWebApplication/Data/DataInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KyhLoginWebApplication.Data;
+
+public class DataInitializer
+{
+    private readonly ApplicationDbContext _context;
+
+    public DataInitializer(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void SeedData()
+    {
+        _context.Database.Migrate();
+        //Seeda data - skapa roller, skapa users EFTER RASTEN
+    }
+}
